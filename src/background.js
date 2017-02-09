b2cdf085dad13a1847c4a01cc3a47560d7c038b9
Peter Loomis <loomispeter@gmail.com>
2017-02-09 13:54:18 -0500

    background script

---
+++ src/background.js
@@ -0,0 +1,7 @@
+import {wrapStore} from 'react-chrome-redux';
+
+// based on https://github.com/tshaddix/react-chrome-redux/wiki/Getting-Started
+
+const store;
+
+wrapStore(store, {portName: 'notist'});
