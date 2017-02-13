818bf8a6809df626a0aec197c2c4de48f605668c
Peter Loomis <loomispeter@gmail.com>
2017-02-12 23:10:03 -0500

    inject annotate button

--- src/content.js
+++ src/content.js
@@ -4,8 +4,24 @@ import {addAnnotation} from './actions';
 const store = new Store({portName: 'notist'});
 
 document.onmouseup = function (e) {
-  var text = document.getSelection().toString();
-  console.log(text);
-  store.dispatch(addAnnotation(text));
-  console.log(store.getState());
+  var text = document.getSelection();
+  if (text.toString().length != 0) {
+    var annotateButton = document.createElement("button");
+    var textNode = document.createTextNode("Annotate");
+    annotateButton.appendChild(textNode);
+    annotateButton.addEventListener("click", () => {
+      console.log(text.toString());
+      store.dispatch(addAnnotation(text));
+      console.log(store.getState());
+    });
+    annotateButton.setAttribute("id", "annotate-button");
+    text.anchorNode.parentNode.appendChild(annotateButton);
+  }
+}
+
+document.onmousedown = function (e) {
+  var annotateButton = document.getElementById("annotate-button");
+  if (annotateButton) {
+    annotateButton.remove();
+  }
 }
