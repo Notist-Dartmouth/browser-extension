30af746841577e367356d9cd521151410e672988
Mark Erwin <spserwinm@gmail.com>
2017-02-27 16:45:23 -0500

    Revert "Got the extension to toggle bold, italics, and underline for selected text"
    This reverts commit 0d81da7d1217e7f5a9d87f323e1b33a2d5f7efbc.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import $ from 'jquery';
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -8,62 +7,12 @@ import NavigationChevronRight from 'material-ui/svg-icons/navigation/chevron-rig
 import IconButton from 'material-ui/IconButton';
 import Annotations from '../containers/Annotations';
 
-const styles = {
-  editableDiv: {
-    border: '1px solid #ddd',
-    minHeight: '100px',
-    padding: '5px',
-    fontFamily: 'sans-serif',
-  },
-};
-
-class ToolBar extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
-    this.onClickB = this.handleClickB.bind(this);
-    this.onClickI = this.handleClickI.bind(this);
-    this.onClickU = this.handleClickU.bind(this);
-  }
-
-  //  Ideally there should be one function that will look at the data-cmd field and use that in execCommand
-  //  Like this: var cmd = $(this).data.cmd;
-  //  document.execCommand(cmd, false, null);
-
-  handleClickB(event) {
-    document.execCommand('bold', false, null);
-  }
-
-  handleClickI(event) {
-    document.execCommand('italic', false, null);
-  }
-
-  handleClickU(event) {
-    document.execCommand('underline', false, null)
-  }
-
-  render() {
-    return (
-      <div>
-        <a  id="datacmd" data-cmd="bold" href="#" onClick={this.onClickB}><strong>B</strong></a>
-        <a id="datacmd" data-cmd="italic" href="#" onClick={this.onClickI}><i>I</i></a>
-        <a id="datacmd" data-cmd="underline" href="#" onClick={this.onClickU}><u>U</u></a>
-        <div id="editable" contentEditable="true" style={styles.editableDiv}>
-          Write here!
-        </div>
-      </div>
-    );
-  }
-}
-
-
 export default class Sidebar extends React.Component {
 
   constructor(props) {
     super(props);
     injectTapEventPlugin();
-    this.state = { open: false };
+    this.state = { open: false }
   }
 
   render() {
@@ -77,15 +26,13 @@ export default class Sidebar extends React.Component {
           <AppBar
             title="Notist"
             iconElementLeft={
-              <IconButton onClick={() => this.setState({ open: !this.state.open })}>
+              <IconButton onClick={() => this.setState({open: !this.state.open})}>
                 {this.state.open ? <NavigationChevronRight /> : <NavigationChevronLeft />}
               </IconButton>
             }
           />
           <Annotations />
-          <ToolBar />
         </Drawer>
-
       </div>
     );
   }
