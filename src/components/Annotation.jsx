c0375cff29b5126b67a7f0a03874505d4d606caf
Mark Erwin <spserwinm@gmail.com>
2017-03-01 17:43:20 -0500

    Added lines that follow indented comments

--- src/components/Annotation.jsx
+++ src/components/Annotation.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { ListItem } from 'material-ui/List';
 import CommentBox from './CommentBox';
 
-const Annotation = (props) => {
+const Annotation = (props) => { //Can go to comment depth of 7
   return (
     <ListItem
       style={{ paddingLeft: 20 * props.depth }}
@@ -10,6 +10,7 @@ const Annotation = (props) => {
       secondaryText={<CommentBox onCommentPost={props.onCommentPost} parentId={props.id} />}
       nestedItems={props.childAnnotations.map(a =>
         <Annotation {...a} key={a.id} depth={props.depth + 1} onCommentPost={props.onCommentPost} />)}
+      nestedListStyle={{ marginLeft: 20, borderLeft: '1px dashed black' }}
     />
   );
 };
