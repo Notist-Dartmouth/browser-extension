43fe4880157a69c8e8ce95a7c80fcc0d546dd437
Mark Erwin <spserwinm@gmail.com>
2017-03-01 16:35:13 -0500

    Nested comments

--- src/components/Annotation.jsx
+++ src/components/Annotation.jsx
@@ -5,10 +5,11 @@ import CommentBox from './CommentBox';
 const Annotation = (props) => {
   return (
     <ListItem
+      style={{ paddingLeft: 20 * props.depth }}
       primaryText={props.articleText || props.text}
       secondaryText={<CommentBox onCommentPost={props.onCommentPost} parentId={props.id} />}
       nestedItems={props.childAnnotations.map(a =>
-        <Annotation {...a} key={a.id} onCommentPost={props.onCommentPost} />)}
+        <Annotation {...a} key={a.id} depth={props.depth + 1} onCommentPost={props.onCommentPost} />)}
     />
   );
 };
@@ -16,6 +17,7 @@ const Annotation = (props) => {
 Annotation.propTypes = {
   articleText: PropTypes.string,
   text: PropTypes.string,
+  depth: PropTypes.number,
   childAnnotations: PropTypes.arrayOf(PropTypes.object),
   onCommentPost: PropTypes.func.isRequired,
   id: PropTypes.string.isRequired,
@@ -24,6 +26,7 @@ Annotation.propTypes = {
 Annotation.defaultProps = {
   articleText: '',
   text: '',
+  depth: 0,
   childAnnotations: [],
 };
 
