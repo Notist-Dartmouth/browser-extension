a75098ce4c3ff2fc9db97b23634ec571fe425602
Peter Loomis <loomispeter@gmail.com>
2017-03-21 15:35:11 -0700

    update test

--- tests/actions.test.js
+++ tests/actions.test.js
@@ -5,14 +5,21 @@ import * as actions from '../src/actions';
 describe('actions', () => {
   it('should create action for creating an annotation', () => {
     const articleText = 'valar morghulis';
+    const ranges = [{
+      end: '/p[69]/span/span',
+      endOffset: 12,
+      start: '/p[70]/span/span',
+      startOffset: 1,
+    }];
     const text = 'valar dohaeris';
     const parent = 4;
     const action = {
       type: types.CREATE_ANNOTATION,
       articleText,
+      ranges,
       text,
       parent,
     };
-    expect(actions.createAnnotation(parent, articleText, text)).toEqual(action);
+    expect(actions.createAnnotation(parent, articleText, ranges, text)).toEqual(action);
   });
 });
