aa04765bc88ebffa540cb306958859bcf4b52e23
Charlotte Blanc <charlotte.blanc@dali.dartmouth.edu>
2017-05-14 16:18:07 -0400

    api calls to backend to update explore data in db

--- src/actions.js
+++ src/actions.js
@@ -285,3 +285,31 @@ export function filterAnnotations(groups) {
     groups,
   };
 }
+
+export function updateUserExploreNum(explore_num, std_dev) {
+  fetch(path.join(apiHost, '/api/user/exploreNumber'), {
+    method: 'POST',
+    credentials: 'include',
+    headers,
+    body: JSON.stringify({ // TODO: does this work?
+      explore_num,
+      std_dev,
+    }),
+  }).then(res => res.json())
+  .then((user) => {
+    console.log(user);
+  });
+}
+
+export function postFbPageArticles(pages, score) {
+  fetch(path.join(apiHost, '/api/initializeExplore/articles'), {
+    method: 'POST',
+    credentials: 'include',
+    headers,
+    body: JSON.stringify({
+    }),
+  }).then(res => res.josn())
+  .then((res) => {
+    console.log(res);
+  });
+}
